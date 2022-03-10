5fe93b03c3d863fb6826047e2eb7005646782521
Junichi Kato <j5ik2o@gmail.com>
2022-03-10 12:42:24 +0900

    wip

--- src/composite/enum_base.rs
+++ src/composite/enum_base.rs
@@ -2,65 +2,111 @@ use std::cell::RefCell;
 use std::fmt::{Display, Formatter};
 use std::rc::Rc;
 
+struct File {
+  name: String,
+  size: usize,
+}
+
+pub struct Directory {
+  name: String,
+  entries: Vec<Rc<RefCell<Entry>>>,
+}
+
 pub enum Entry {
-  File {
-    name: String,
-    size: usize,
-  },
-  Directory {
-    name: String,
-    entries: Vec<Rc<RefCell<Entry>>>,
-  },
+  FileEntry(File),
+  DirectoryEntry(Directory),
 }
 
 impl Display for Entry {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
     match self {
-      Entry::File { name, size } => write!(f, "{} ({})", name, size),
-      Entry::Directory { name, .. } => write!(f, "{} ({})", name, self.get_size()),
+      Entry::FileEntry(file) => write!(f, "{}", file),
+      Entry::DirectoryEntry(Directory { name, .. }) => write!(f, "{} ({})", name, self.get_size()),
+    }
+  }
+}
+
+impl Display for File {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{} ({})", self.name, self.size)
+  }
+}
+
+impl File {
+  pub fn get_name(&self) -> &str {
+    &self.name
+  }
+
+  pub fn get_size(&self) -> usize {
+    self.size
+  }
+
+  fn print_line_with_prefix(&self, prefix: &str) {
+    println!("{}/{}", prefix, self);
+  }
+}
+
+impl Display for Directory {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{} ({})", self.name, self.get_size())
+  }
+}
+
+impl Directory {
+  pub fn get_name(&self) -> &str {
+    &self.name
+  }
+
+  pub fn add(&mut self, entry: Rc<RefCell<Entry>>) {
+    self.entries.push(entry);
+  }
+
+  pub fn get_size(&self) -> usize {
+    self.entries.iter().fold(0, |r, e| r + e.borrow().get_size())
+  }
+
+  fn print_line_with_prefix(&self, prefix: &str) {
+    println!("{}/{}", prefix, self);
+    for entry in &self.entries {
+      let entry_ref = (&**entry).borrow();
+      entry_ref.print_line_with_prefix(&format!("{}/{}", prefix, self.name))
     }
   }
 }
 
 impl Entry {
   fn of_file(name: &str, size: usize) -> Self {
-    Entry::File {
+    Entry::FileEntry(File {
       name: name.to_owned(),
       size,
-    }
+    })
   }
 
   fn of_directory(name: &str) -> Self {
-    Entry::Directory {
+    Entry::DirectoryEntry(Directory {
       name: name.to_owned(),
       entries: vec![],
-    }
+    })
   }
 
   fn print_line_with_prefix(&self, prefix: &str) {
     match self {
-      Entry::File { .. } => println!("{}/{}", prefix, self),
-      Entry::Directory { name, entries } => {
-        println!("{}/{}", prefix, self);
-        for entry in entries {
-          let entry_ref = (&**entry).borrow();
-          entry_ref.print_line_with_prefix(&format!("{}/{}", prefix, name))
-        }
-      }
+      Entry::FileEntry(f) => f.print_line_with_prefix(prefix),
+      Entry::DirectoryEntry(d) => d.print_line_with_prefix(prefix),
     }
   }
 
   pub fn get_name(&self) -> &str {
     match self {
-      Entry::File { name, .. } => name,
-      Entry::Directory { name, .. } => name,
+      Entry::FileEntry(f) => f.get_name(),
+      Entry::DirectoryEntry(d) => d.get_name(),
     }
   }
 
   pub fn get_size(&self) -> usize {
     match self {
-      Entry::File { size, .. } => *size,
-      Entry::Directory { entries, .. } => entries.iter().fold(0, |r, e| r + e.borrow().get_size()),
+      Entry::FileEntry(f) => f.get_size(),
+      Entry::DirectoryEntry(d) => d.get_size(),
     }
   }
 
@@ -68,11 +114,24 @@ impl Entry {
     self.print_line_with_prefix("");
   }
 
-  // CAUTION: addを実装することを正しいとした場合は、Entry::FileにおいてLSP違反の疑いあり！
-  pub fn add(&mut self, entry: Rc<RefCell<Entry>>) {
+  pub fn as_file(&self) -> Option<&File> {
     match self {
-      Entry::File { .. } => panic!("Unsupported operation!!!"),
-      Entry::Directory { entries, .. } => entries.push(entry),
+      Entry::FileEntry(d) => Some(d),
+      _ => None,
+    }
+  }
+
+  pub fn as_directory(&self) -> Option<&Directory> {
+    match self {
+      Entry::DirectoryEntry(d) => Some(d),
+      _ => None,
+    }
+  }
+
+  pub fn as_directory_mut(&mut self) -> Option<&mut Directory> {
+    match self {
+      Entry::DirectoryEntry(d) => Some(d),
+      _ => None,
     }
   }
 }
@@ -91,14 +150,20 @@ mod test {
     let tmpdir = Rc::new(RefCell::new(Entry::of_directory("tmp")));
     let usrdir = Rc::new(RefCell::new(Entry::of_directory("usr")));
 
-    rootdir.add(bindir.clone());
-    rootdir.add(tmpdir.clone());
-    rootdir.add(usrdir.clone());
+    rootdir.as_directory_mut().unwrap().add(bindir.clone());
+    rootdir.as_directory_mut().unwrap().add(tmpdir.clone());
+    rootdir.as_directory_mut().unwrap().add(usrdir.clone());
 
     {
       let mut bindir_ref = (&*bindir).borrow_mut();
-      bindir_ref.add(Rc::new(RefCell::new(Entry::of_file("vi", 10000))));
-      bindir_ref.add(Rc::new(RefCell::new(Entry::of_file("latex", 20000))));
+      bindir_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("vi", 10000))));
+      bindir_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("latex", 20000))));
       // bindir_ref.print_line();
     }
 
@@ -108,29 +173,44 @@ mod test {
 
     {
       let mut usrdir_ref = (&*usrdir).borrow_mut();
-      usrdir_ref.add(yuki.clone());
-      usrdir_ref.add(hanako.clone());
-      usrdir_ref.add(tomura.clone());
+      usrdir_ref.as_directory_mut().unwrap().add(yuki.clone());
+      usrdir_ref.as_directory_mut().unwrap().add(hanako.clone());
+      usrdir_ref.as_directory_mut().unwrap().add(tomura.clone());
       // usrdir_ref.print_line();
     }
 
     {
       let mut yuki_ref = (&*yuki).borrow_mut();
-      yuki_ref.add(Rc::new(RefCell::new(Entry::of_file("diary.html", 100))));
-      yuki_ref.add(Rc::new(RefCell::new(Entry::of_file("Composite.java", 200))));
+      yuki_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("diary.html", 100))));
+      yuki_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("Composite.java", 200))));
       // yuki_ref.print_line();
     }
 
     {
       let mut hanako_ref = (&*hanako).borrow_mut();
-      hanako_ref.add(Rc::new(RefCell::new(Entry::of_file("memo.tex", 300))));
+      hanako_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("memo.tex", 300))));
       // hanako_ref.print_line();
     }
 
     {
       let mut tomura_ref = (&*tomura).borrow_mut();
-      tomura_ref.add(Rc::new(RefCell::new(Entry::of_file("game.doc", 400))));
-      tomura_ref.add(Rc::new(RefCell::new(Entry::of_file("junk.mail", 500))));
+      tomura_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("game.doc", 400))));
+      tomura_ref
+        .as_directory_mut()
+        .unwrap()
+        .add(Rc::new(RefCell::new(Entry::of_file("junk.mail", 500))));
       // tomura_ref.print_line();
     }
 
