1d1eda74a254a14c6f9fcfbd1d25ecdd8065d962
Junichi Kato <j5ik2o@gmail.com>
2022-03-11 14:07:59 +0900

    wip

--- src/observer/enum_base.rs
+++ src/observer/enum_base.rs
@@ -1,7 +1,7 @@
 use rand::prelude::ThreadRng;
 use rand::Rng;
 use std::borrow::{Borrow, BorrowMut};
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::fmt::Debug;
 use std::rc::Rc;
 use std::{thread, time};
@@ -11,9 +11,27 @@ pub enum NumberGenerator {
 }
 
 impl NumberGenerator {
+  pub fn of_random() -> Self {
+    NumberGenerator::RandomNumber(RandomNumberNumberGenerator::new())
+  }
+
   pub fn get_number(&self) -> u32 {
     match self {
       NumberGenerator::RandomNumber(g) => g.get_number(),
+      _ => panic!(),
+    }
+  }
+
+  pub fn add_observer(&mut self, observer: Observer) {
+    match self {
+      NumberGenerator::RandomNumber(g) => g.add_observer(observer),
+      _ => panic!(),
+    }
+  }
+
+  pub fn execute(&mut self) {
+    match self {
+      NumberGenerator::RandomNumber(g) => g.execute(),
       _ => panic!(),
     }
   }
@@ -31,6 +49,16 @@ struct RandomNumberNumberGeneratorInner {
 }
 
 impl RandomNumberNumberGenerator {
+  pub fn new() -> Self {
+    Self {
+      inner: Rc::new(RefCell::new(RandomNumberNumberGeneratorInner {
+        observers: vec![],
+        rng: rand::thread_rng(),
+        number: 0,
+      })),
+    }
+  }
+
   fn add_observer(&mut self, observer: Observer) {
     let mut g = (&*self.inner).borrow_mut();
     g.observers.push(observer);
@@ -67,6 +95,7 @@ impl RandomNumberNumberGenerator {
     for _ in 0..20 {
       let mut g = (&*self.inner).borrow_mut();
       g.number = g.rng.gen_range(0, 49);
+      drop(g);
       self.notify_observers();
     }
   }
@@ -101,5 +130,19 @@ impl Observer {
       }
       Observer::Any(rc) => rc.update(generator),
     }
+  }
+}
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test() {
+    let mut generator = NumberGenerator::of_random();
+    let observer1 = Observer::Digit;
+    let observer2 = Observer::Graph;
+    generator.add_observer(observer1);
+    generator.add_observer(observer2);
+    generator.execute();
   }
 }
