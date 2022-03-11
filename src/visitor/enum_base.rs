8acb275733b8b7b5ca8844c64aa1a70d738954e3
Junichi Kato <j5ik2o@gmail.com>
2022-03-12 08:47:27 +0900

    wip

---
+++ src/visitor/enum_base.rs
@@ -0,0 +1,171 @@
+pub enum Visitor {
+  HtmlExporterVisitor { builder: String },
+  PlainTextExporterVisitor { builder: String },
+}
+
+impl Visitor {
+  pub fn get_result(&self) -> &str {
+    match self {
+      Visitor::HtmlExporterVisitor { builder } => builder,
+      Visitor::PlainTextExporterVisitor { builder } => builder,
+    }
+  }
+
+  pub fn of_html_exporter() -> Self {
+    Visitor::HtmlExporterVisitor { builder: "".to_owned() }
+  }
+
+  pub fn of_plain_text_exporter() -> Self {
+    Visitor::PlainTextExporterVisitor { builder: "".to_owned() }
+  }
+
+  pub fn visit_title(&mut self, title: &Title) {
+    match self {
+      Visitor::HtmlExporterVisitor { builder } => builder.push_str(&format!("<h1>{}</h1>\n", title.text)),
+      Visitor::PlainTextExporterVisitor { builder } => builder.push_str(&format!("{}\n", title.text)),
+    }
+  }
+
+  pub fn visit_text(&mut self, text: &Text) {
+    match self {
+      Visitor::HtmlExporterVisitor { builder } => builder.push_str(&format!("<p>{}</p>\n", text.text)),
+      Visitor::PlainTextExporterVisitor { builder } => builder.push_str(&format!("{}\n", text.text)),
+    }
+  }
+
+  pub fn visit_hyperlink(&mut self, hyperlink: &HyperLink) {
+    match self {
+      Visitor::HtmlExporterVisitor { builder } => {
+        builder.push_str(&format!("<a href=\"{}\">{}</a>\n", hyperlink.url, hyperlink.text))
+      }
+      Visitor::PlainTextExporterVisitor { builder } => {
+        builder.push_str(&format!("{} {}\n", hyperlink.text, hyperlink.url))
+      }
+    }
+  }
+}
+
+#[derive(Debug)]
+pub enum Element {
+  Title(Title),
+  Text(Text),
+  HyperLink(HyperLink),
+}
+
+impl Element {
+  pub fn of_title(s: &str) -> Self {
+    Element::Title(Title::new(s))
+  }
+
+  pub fn of_text(s: &str) -> Self {
+    Element::Text(Text::new(s))
+  }
+
+  pub fn of_hyper_link(text: &str, url: &str) -> Self {
+    Element::HyperLink(HyperLink::new(text, url))
+  }
+
+  pub fn accept(&self, visitor: &mut Visitor) {
+    match self {
+      Element::Title(inner) => inner.accept(visitor),
+      Element::Text(inner) => inner.accept(visitor),
+      Element::HyperLink(inner) => inner.accept(visitor),
+    }
+  }
+}
+
+#[derive(Debug)]
+pub struct Title {
+  text: String,
+}
+
+impl Title {
+  pub fn new(text: &str) -> Self {
+    Self { text: text.to_owned() }
+  }
+
+  pub fn accept(&self, visitor: &mut Visitor) {
+    visitor.visit_title(self)
+  }
+}
+
+#[derive(Debug)]
+pub struct Text {
+  text: String,
+}
+
+impl Text {
+  pub fn new(text: &str) -> Self {
+    Self { text: text.to_owned() }
+  }
+
+  pub fn accept(&self, visitor: &mut Visitor) {
+    visitor.visit_text(self)
+  }
+}
+
+#[derive(Debug)]
+pub struct HyperLink {
+  text: String,
+  url: String,
+}
+
+impl HyperLink {
+  pub fn new(text: &str, url: &str) -> Self {
+    Self {
+      text: text.to_owned(),
+      url: url.to_owned(),
+    }
+  }
+
+  pub fn accept(&self, visitor: &mut Visitor) {
+    visitor.visit_hyperlink(self)
+  }
+}
+
+#[derive(Debug)]
+pub struct Document {
+  parts: Vec<Element>,
+}
+
+impl Document {
+  pub fn new(parts: impl IntoIterator<Item = Element>) -> Self {
+    Self {
+      parts: parts.into_iter().collect(),
+    }
+  }
+
+  pub fn accept(&self, visitor: &mut Visitor) {
+    for e in &self.parts {
+      e.accept(visitor)
+    }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use std::borrow::Borrow;
+
+  #[test]
+  fn test() {
+    let values: Vec<Element> = vec![
+      Element::of_title("The Visitor Pattern Example"),
+      Element::of_text("The visitor pattern helps us add extra functionality without changing the classes."),
+      Element::of_hyper_link("Go check it online!", "https://www.google.com/"),
+      Element::of_text("Thanks!"),
+    ];
+
+    let document = Document::new(values);
+    let mut html_exporter = Visitor::of_html_exporter();
+    let mut plain_text_exporter = Visitor::of_plain_text_exporter();
+
+    println!("Export to html:");
+    document.accept(&mut html_exporter);
+    println!("{}", html_exporter.get_result());
+
+    println!("Export to plain:");
+    document.accept(&mut plain_text_exporter);
+    println!("{}", plain_text_exporter.get_result());
+  }
+}
---
+++ src/visitor/trait_base.rs
@@ -0,0 +1,181 @@
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::fmt::Debug;
+use std::rc::Rc;
+
+pub trait Visitor {
+  fn visit_title(&mut self, title: &Title);
+  fn visit_text(&mut self, text: &Text);
+  fn visit_hyperlink(&mut self, hyperlink: &HyperLink);
+}
+
+pub trait Element: Debug {
+  fn accept(&self, visitor: Rc<RefCell<dyn Visitor>>);
+}
+
+#[derive(Debug)]
+pub struct Title {
+  text: String,
+}
+
+impl Title {
+  pub fn new(text: &str) -> Self {
+    Self { text: text.to_owned() }
+  }
+}
+
+impl Element for Title {
+  fn accept(&self, visitor: Rc<RefCell<dyn Visitor>>) {
+    (&*visitor).borrow_mut().visit_title(self)
+  }
+}
+
+#[derive(Debug)]
+pub struct Text {
+  text: String,
+}
+
+impl Text {
+  pub fn new(text: &str) -> Self {
+    Self { text: text.to_owned() }
+  }
+}
+
+impl Element for Text {
+  fn accept(&self, visitor: Rc<RefCell<dyn Visitor>>) {
+    (&*visitor).borrow_mut().visit_text(self)
+  }
+}
+
+#[derive(Debug)]
+pub struct HyperLink {
+  text: String,
+  url: String,
+}
+
+impl HyperLink {
+  pub fn new(text: &str, url: &str) -> Self {
+    Self {
+      text: text.to_owned(),
+      url: url.to_owned(),
+    }
+  }
+}
+
+impl Element for HyperLink {
+  fn accept(&self, visitor: Rc<RefCell<dyn Visitor>>) {
+    (&*visitor).borrow_mut().visit_hyperlink(self)
+  }
+}
+
+#[derive(Debug)]
+pub struct Document {
+  parts: Vec<Rc<dyn Element>>,
+}
+
+impl Document {
+  pub fn new(parts: impl IntoIterator<Item = Rc<dyn Element>>) -> Self {
+    Self {
+      parts: parts.into_iter().collect(),
+    }
+  }
+
+  pub fn accept(&self, visitor: Rc<RefCell<dyn Visitor>>) {
+    for e in &self.parts {
+      e.accept(visitor.clone())
+    }
+  }
+}
+
+#[derive(Debug)]
+pub struct HtmlExporterVisitor {
+  builder: String,
+}
+
+impl HtmlExporterVisitor {
+  pub fn new() -> Self {
+    Self { builder: "".to_owned() }
+  }
+
+  pub fn get_html(&self) -> &str {
+    &self.builder
+  }
+}
+
+impl Visitor for HtmlExporterVisitor {
+  fn visit_title(&mut self, title: &Title) {
+    self.builder.push_str(&format!("<h1>{}</h1>\n", title.text));
+  }
+
+  fn visit_text(&mut self, text: &Text) {
+    self.builder.push_str(&format!("<p>{}</p>\n", text.text));
+  }
+
+  fn visit_hyperlink(&mut self, hyperlink: &HyperLink) {
+    self
+      .builder
+      .push_str(&format!("<a href=\"{}\">{}</a>\n", hyperlink.url, hyperlink.text));
+  }
+}
+
+#[derive(Debug)]
+pub struct PlainTextExporterVisitor {
+  builder: String,
+}
+
+impl PlainTextExporterVisitor {
+  pub fn new() -> Self {
+    Self { builder: "".to_owned() }
+  }
+
+  pub fn get_text(&self) -> &str {
+    &self.builder
+  }
+}
+
+impl Visitor for PlainTextExporterVisitor {
+  fn visit_title(&mut self, title: &Title) {
+    self.builder.push_str(&format!("{}\n", title.text));
+  }
+
+  fn visit_text(&mut self, text: &Text) {
+    self.builder.push_str(&format!("{}\n", text.text));
+  }
+
+  fn visit_hyperlink(&mut self, hyperlink: &HyperLink) {
+    self
+      .builder
+      .push_str(&format!("{} {}\n", hyperlink.text, hyperlink.url));
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use std::borrow::Borrow;
+
+  #[test]
+  fn test() {
+    let values: Vec<Rc<dyn Element>> = vec![
+      Rc::new(Title::new("The Visitor Pattern Example")),
+      Rc::new(Text::new(
+        "The visitor pattern helps us add extra functionality without changing the classes.",
+      )),
+      Rc::new(HyperLink::new("Go check it online!", "https://www.google.com/")),
+      Rc::new(Text::new("Thanks!")),
+    ];
+    let document = Document::new(values);
+
+    let html_exporter = Rc::new(RefCell::new(HtmlExporterVisitor::new()));
+
+    let plain_text_exporter = Rc::new(RefCell::new(PlainTextExporterVisitor::new()));
+
+    println!("Export to html:");
+    document.accept(html_exporter.clone());
+    println!("{}", (&*html_exporter).borrow().get_html());
+
+    println!("Export to plain:");
+    document.accept(plain_text_exporter.clone());
+    println!("{}", (&*plain_text_exporter).borrow().get_text());
+  }
+}
